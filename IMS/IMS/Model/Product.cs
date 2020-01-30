f2dc73f68af7d5f46857e52b4b1a8b6fca254d19
patrick.gilstad@hiof.no <patrick.gilstad@hiof.no>
2020-01-30 14:01:52 +0000

    Migrating Code
    Migrating Code from test application - App can extract text from JSON file. Extracting images from JSON will be added

---
+++ IMS/IMS/Model/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public string desc { get; set; }
+        //public BitmapImage img { get; set; }
+        public string img { get; set; }
+    }
+
+    public class ProductList
+    {
+        public List<Product> product { get; set; }
+    }
+
+
+}
